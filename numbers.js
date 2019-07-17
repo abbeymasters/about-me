0b74c78a2032aba584c37cb17fa61d8247d9dfc8
Abbey Masters <abbeykmasters@gmail.com>
2019-07-16 17:59:30 -0700

    adding

---
+++ numbers.js
@@ -0,0 +1,8 @@
+// Dom Elements
+
+const button = document.getElementById('button');
+const input = document.getElementById('inputnumber');
+
+const tooHigh = document.getElementById('toohigh');
+const tooLow = document.getElementById('toolow');
+const youWin = document.getElementById('youwin');

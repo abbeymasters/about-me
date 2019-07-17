a46db81c5999b7523dd7d09f0fc909a1117fe166
Abbey Masters <abbeykmasters@gmail.com>
2019-07-17 00:18:43 -0700

    adding tests

--- Test/isYes-test.js
+++ Test/isYes-test.js
@@ -1,10 +1,20 @@
-export default isYes;
+import isYes from '../isYes.js'; 
 
-function isYes(text) {
-    if(text === 'abbey') {
-        return true;
-    } else if(text === 'Abbey') {
-        return true;
-    } else { 
-        return false;
-    }}
+const test = QUnit.test;
+
+test('abbey', function(assert) {
+// Arrange
+// Set up your parameters and expectations
+
+    const text = 'kabir';
+    const expected = false;
+
+// Act
+// Call the function you’re testing and set the result to a const
+
+    const result = isYes(text);
+
+// Assert
+    assert.equal(result, expected); 
+});
+

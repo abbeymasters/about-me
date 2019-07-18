8f9e893ee8359b95127e366946a45c180eb86304
Abbey Masters <abbeykmasters@gmail.com>
2019-07-18 13:47:06 -0700

    went to previous commit to re-start

--- get-play.js
+++ get-play.js
@@ -1,21 +1,8 @@
-// return rock, paper or scissors - this is the computer's choice
-export default function getPlay
+// return rock, paper or scissors
 
 
-function getCompAnswer() {
-    const computerOptions = ["rock", "paper", "scissors"];
-    const randomNumber = Math.floor((Math.random() * 3) + 1);
-    
-    options.forEach(option => {
-        option.addEventListener('click', function() {
-        const computerChoice  =  computerOptions[randomNumber];
-        return computerChoice;
-    });
-
-
-
-    function getPlay() {
-    // generate number
+export default function getPlay() {
+    // generate 0, 1 or 2
     const randomNumber = Math.floor((Math.random() * 3) + 1);
 
     // then call rock paper or scissor
@@ -25,12 +12,12 @@ function getCompAnswer() {
     return choice;
 }
 
-//export function getRockPaperScissor(randomNumber) {
- //   if(randomNumber === 1) {
-   //     return 'rock';
-  //  } else if(randomNumber === 2) {
-   //     return 'paper';
-   // } else {
-     //   return 'scissors';
-   // }
-//}
+export function getRockPaperScissor(randomNumber) {
+    if(randomNumber === 1) {
+        return 'rock';
+    } else if(randomNumber === 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
--- rps.js
+++ rps.js
@@ -1,6 +1,5 @@
 // Import functions
 import getPlay from './get-play.js';
-import playMatch from './get-play.js';
 
 // Call elements from DOM
 
@@ -13,69 +12,9 @@ const loseCount = document.getElementById('loss-count');
 let wins = 0;
 let losses = 0;
 
-const whichOne = document.querySelector('input:checked');
-
-// CLICK BUTTON --> RANDOMLY CHOOSE ROCK, PAPER, OR SCISSORS --> DISPLAY IMAGE
+// CLICK BUTTON --> RANDOMLY CHOOSE A WINNER --> DISPLAY IMAGE
 
 playButton.addEventListener('click', () => {
-    const compareHands = (whichOne, computerChoice) =>{
-    
-    result.classList.remove('invisible');
-    let src = './src/assets/' + choice + '.png';
-    result.src = src; 
-
-    // THE USER'S CHOICE
-    const tie = whichOne.value === choice;
-
-    
-    // IF THEN
-    if(tie) {
-        message.textContent = 'TIED!';
-        return;   
-    } 
-    
-    if(whichOne.value === 'rock') {
-        if(whichOne.value === 'scissors')
-            message.textContent = 'WON!';
-        wins++;
-        winCount.textContent = wins;    
-    } else {
-        message.textContent = 'LOST!';
-        losses++;
-        loseCount.textContent = losses;
-    }
-
-    if(whichOne.value === 'scissors') {
-        if(whichOne.value === 'paper')
-            message.textContent = 'WON!';
-        wins++;
-        winCount.textContent = wins;    
-    } else {
-        message.textContent = 'LOST!';
-        losses++;
-        loseCount.textContent = losses;
-    }
-
-    if(whichOne.value === 'paper') {
-        if(whichOne.value === 'rock')
-            message.textContent = 'WON!';
-        wins++;
-        winCount.textContent = wins;    
-    } else {
-        message.textContent = 'LOST!';
-        losses++;
-        loseCount.textContent = losses;
-    }
-};
-
-// Bet Button Section — Incomplete
-
-const betButton = document.getElementById('bet-button');
-const betInput = document.getElementById('bet-input');
-const moneyCount = document.getElementById('money-count');
-
-
-betButton.addEventListener('click', () => {
     const choice = getPlay();
     
     result.classList.remove('invisible');
@@ -83,20 +22,16 @@ betButton.addEventListener('click', () => {
     result.src = src; 
 
     const whichOne = document.querySelector('input:checked');
-    const won = whichOne.value === choice;
 
-    let win = 5 + amount;
-    let lose = 5 - amount;
+    const won = whichOne.value === choice;
 
     if(won) {
         message.textContent = 'WON!';
         wins++;
-        winCount.textContent = wins;  
-        moneyCount.textContent = '$' + win;      
+        winCount.textContent = wins;       
     } else {
         message.textContent = 'LOST!';
         losses++;
         loseCount.textContent = losses;
-        moneyCount.textContent = '$' + lose;
     }
 });

b546d98df9d55c1cf6a55ab3e7e8ca1e4ee92b37
Abbey Masters <abbeykmasters@gmail.com>
2019-07-18 23:21:10 -0700

    fixed index.html

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import isYes from './isYes.js/index.js'; 
-import isYes2 from './isyes2.js/index.js'; 
-import isYes3 from './isYes3.js/index.js'; 
+import isYes from '/src/isYes.js'; 
+import isYes2 from '/src/favoritebook.js'; 
+import isYes3 from '/src/born-question.js'; 
 
 // Reference needed DOM elements
 

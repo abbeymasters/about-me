774671b397c6736f5e73d7d656440baa7f5d5c21
Abbey Masters <abbeykmasters@gmail.com>
2019-07-21 18:25:24 -0700

    adding bets

--- src/rps.js
+++ src/rps.js
@@ -12,6 +12,10 @@ const lossCount = document.getElementById('loss-count');
 let wins = 0;
 let losses = 0;
 
+const betButton = document.getElementById('bet-button');
+const moneyCount = document.getElementById('money-count');
+const betInput = document.getElementById('get-input');
+
 
 // return rock, paper or scissors
 
@@ -45,6 +49,71 @@ function compareResults(userChoice, compChoice) {
             resultMessage.textContent = 'WON!';
             wins++;
             winCount.textContent = wins;  
+        } else {
+            resultMessage.textContent = 'LOST!';
+            losses++;
+            lossCount.textContent = losses;
+        } 
+    }
+    else if(userChoice === 'scissors') {
+        if(compChoice === 'paper') {
+            resultMessage.textContent = 'WON!';
+            wins++;
+            winCount.textContent = wins;  
+        } else {
+            resultMessage.textContent = 'LOST!';
+            losses++;
+            lossCount.textContent = losses;
+        } 
+    }
+    else if(userChoice === 'paper') {
+        if(compChoice === 'rock') {
+            resultMessage.textContent = 'WON!';
+            wins++;
+            winCount.textContent = wins;  
+        } else {
+            resultMessage.textContent = 'LOST!';
+            losses++;
+            lossCount.textContent = losses;
+        }
+    }}
+
+    // betting section
+
+betButton.addEventListener('click', () => {
+    // get computer's choice
+    const compChoice = getPlay();
+
+    // get user's Choice
+    let userChoice = document.querySelector('input:checked').value;
+
+    // pull image for winner
+    resultImage.classList.remove('invisible');
+    let src = '/assets/' + compChoice + '.png';
+    resultImage.src = src; 
+
+    compareBetResults(userChoice, compChoice); 
+
+    //send results
+    winCount.textContent = wins;
+    lossCount.textContent = losses;
+
+});   
+
+//compareResults + betting
+
+
+function compareBetResults(userChoice, compChoice) {
+    if(userChoice === compChoice) {
+        resultMessage.textContent = 'TIED!';
+    }
+    else if(userChoice === 'rock') {
+        if(compChoice === 'scissors') {
+            resultMessage.textContent = 'WON!';
+            wins++;
+            winCount.textContent = wins; 
+            let x = moneyCount + betInput;
+            moneyCount.textContent = x;
         } else {
             resultMessage.textContent = 'LOST!';
             losses++;
